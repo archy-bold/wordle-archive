08f6bb7e5e6aa83bd6aabe23788eea8881c78faa
Simon Archer <simon@archybold.com>
2022-02-01 11:19:58 +0000

    Handling input error in the restore bookmarklet.

--- src/load-state.js
+++ src/load-state.js
@@ -3,10 +3,13 @@
     div.setAttribute('id', 'inputDiv');
     div.setAttribute('style', 'position:absolute;top:0;right:0;bottom:0;left:0;z-index:10000;background:white;');
     var btnClick = "var str = window.document.getElementById('gameStateInput').value;"
+        + "try{"
         + "var json = JSON.parse(str);"
+        + "if (json.gameState == null || json.statistics == null) throw 'err';"
         + "window.localStorage.setItem('gameState', JSON.stringify(json.gameState));"
         + "window.localStorage.setItem('statistics', JSON.stringify(json.statistics));"
-        + "window.location.reload()";
+        + "window.location.reload()"
+        + "}catch(err){alert('Can\\\'t read the input.')}";
     div.innerHTML = '<textarea id="gameStateInput" style="width:100%;height:150px;box-sizing:border-box;" placeholder="Paste your save here"></textarea><button onclick="' + btnClick + '">Restore</button>';
     window.document.body.appendChild(div);
 })();
